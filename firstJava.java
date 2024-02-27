4d8f770efd75a9aea3dd32d076a7e653d39ca82c
aditya2204arora <160465285+aditya2204arora@users.noreply.github.com>
2024-02-27 02:17:20 -0800

    Update firstJava.java

--- firstJava.java
+++ firstJava.java
@@ -2,6 +2,6 @@ public class firstJava
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello! Welcome to Jenkins");
+		System.out.println("Hello! Welcome to Jenkins! Demonstrating Freestyle");
 	}
 }
